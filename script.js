46f85ed4faafb47b2a082c4fe2f27cf3b64676bd
jtn45~ <jtn45i@tutanota.com>
2022-03-22 18:42:20 +0200

    https://github.com/jtn45i/rock-paper-scissor

---
+++ script.js
@@ -0,0 +1,8 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+let buttons = document.querySelectorAll("selection");
+let maxNumber;
+for(let button of buttons){
+    button.addEventListener("click", e => {
+        userChoice = e.target.innerText;
+    })
+}
